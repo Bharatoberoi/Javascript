c710474dd9d77dc1db718e9a886e59dda8dc670b
Bharat <bharat.oberoi.9@gmail.com>
2024-04-11 17:37:38 +0530

    Create promisechaining.js

---
+++ 15Promises/promisechaining.js
@@ -0,0 +1,23 @@
+let p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+            console.log("Resolved after 2 seconds")
+            resolve(56)
+    }, 2000)
+})
+
+p1.then((value) => {
+    console.log(value)
+    return new Promise((resolve, reject) => {
+            setTimeout(() => { resolve("Promise 2") }, 2000)
+    }) 
+}).then((value) => {
+    console.log("We are done")
+    return 2
+}).then((value)=>{
+    console.log("Now we are pakka done")
+})
+
+//Resolved after 2 seconds
+//Promise 2
+//We are done
+//Now we are pakka done
